7e5939b17ce45d78426e8f9303ea93c337c1ca85
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2023-02-15 07:50:09 -0800

    main: more meta update

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       <link rel="icon" href="/favicon.ico" />
 
       <meta property="og:type" content="website" />
