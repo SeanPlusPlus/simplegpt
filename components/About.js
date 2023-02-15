6cd6efb5a53202cae4bfc9fb3b9197642f96bc95
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2023-02-15 08:35:32 -0800

    main: about update

--- components/About.js
+++ components/About.js
@@ -27,7 +27,7 @@ const About = () => {
             The idea here is really straightforward. The web app is simply asking the <a className="link link-primary" href="https://beta.openai.com/docs/guides/completion/prompt-design" target="_blank" rel="noopener noreferer">OpenAI text completion API</a> to explain your input in simple terms.
           </p>
           <p className="pb-5">
-            Now, why did I build this? Great question! Mostly, I just love to mess around with prototypes and deploy little hacks so that I can use cool new tech. But in this case, in addition to using new tools, I often wanna just as chatgpt to explain something in simple terms, and this site gives me a blazingly fast an easy way to do so without auth!
+            Now, why did I build this? Great question! Mostly, I just love to mess around with prototypes and deploy little hacks so that I can use cool new tech. But in this case, in addition to using new tools, I often wanna just as chatgpt to explain something in simple terms, and this site gives me a blazingly fast and easy way to do so without having to authenticate first!
           </p>
           <p className="pb-5">
             Hit me up, and let me know how you like it! <a className="link link-primary" href="https://twitter.com/SeanPlusPlus/" target="_blank" rel="noopener noreferrer">@SeanPlusPlus</a>
