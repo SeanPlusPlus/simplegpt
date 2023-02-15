ae90a7faaaab08f9f28dd003ba0e326108a8d26f
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2023-02-15 07:42:48 -0800

    main: meta update

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 
-const title = "Simple GPT Foo"
-const description = "Ask GPT to explain something in simple terms"
+const title = "Simple GPT"
+const description = "Ask GPT to explain anything in simple terms"
+const domain = "simple-gpt.vercel.app"
+const site = "https://simple-gpt.vercel.app/"
+const image = `${site}/simple.png`
 
 export default function Header() {
   return (
@@ -10,6 +13,19 @@ export default function Header() {
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={site} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:domain" value={domain} />
+      <meta name="twitter:title" value={title} />
+      <meta name="twitter:description" value={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:url" value={site} />
     </Head>
   )
 }
