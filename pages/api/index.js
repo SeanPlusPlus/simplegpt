f62fd7db1c102c18da0ec4d3a2ef4040ba1e9a41
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2023-03-24 08:41:28 -0700

    main: slack

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,18 +1,22 @@
 import _get from 'lodash/get'
+import axios from 'axios'
 import { Configuration, OpenAIApi } from 'openai'
 
 require('dotenv').config()
 
+// Slack
+const SLACK_WEB_HOOK_URL = process.env.SLACK_WEB_HOOK_URL
+
+// OpenAI
 const MAX_TOKENS = 1024
 const TEMPERATURE = 0.1
 const MODEL = 'text-davinci-003'
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
-
 const openai = new OpenAIApi(configuration)
 
+// handler
 const handleInput = async (req, res) => {
   if (req.method !== 'POST') {
     res.status(405).send({ message: 'Only POST requests allowed' })
@@ -37,6 +41,13 @@ const handleInput = async (req, res) => {
   })
 
   const output = baseCompletion.data.choices.pop()
+
+  if (SLACK_WEB_HOOK_URL) {
+    const slackData = JSON.stringify({
+      text: output.text
+    })
+    await axios.post(SLACK_WEB_HOOK_URL, slackData)
+  }
 
   res.status(200).json({
     received: inputText,
