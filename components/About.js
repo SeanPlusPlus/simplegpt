28dbe776005f4c6932075eaf3f5ea2267753413a
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2023-03-24 08:10:58 -0700

    main: description

--- components/About.js
+++ components/About.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import { APP_TITLE } from '../constants'
+import Description from './Description'
 
 const About = () => {
   const {
@@ -22,15 +23,7 @@ const About = () => {
           </span>
         </h3>
         <div>
-          <p className="pb-5">
-            The idea here is really straightforward. The web app is simply asking the <a className="link link-primary" href="https://beta.openai.com/docs/guides/completion/prompt-design" target="_blank" rel="noopener noreferer">OpenAI text completion API</a> to explain your input in simple terms.
-          </p>
-          <p className="pb-5">
-            Now, why did I build this? Great question! Mostly, I just love to mess around with prototypes and deploy little hacks so that I can use cool new tech. But in this case, in addition to using new tools, I often wanna just ask chatgpt to explain something in simple terms, and this site gives me a blazingly fast and easy way to do so without having to authenticate first!
-          </p>
-          <p className="pb-5">
-            Hit me up, and let me know how you like it! <a className="link link-primary" href="https://twitter.com/SeanPlusPlus/" target="_blank" rel="noopener noreferrer">@SeanPlusPlus</a>
-          </p>
+          <Description />
         </div>
         <div className="modal-action pt-5">
           <label htmlFor="my-modal" className="btn" onClick={handleClose}>Close</label>
